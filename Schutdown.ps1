ab6f8f3b6bec49b9e6711dc7bac47929f5b8e3a3
TheScreaMingGoaT <paulpaul123paul123@gmail.com>
2024-02-06 10:51:12 +0100

    daraus kann man was machen

---
+++ Schutdown.ps1
@@ -0,0 +1,6 @@
+﻿$action = New-ScheduledTaskAction -Execute "shutdown" -Argument "/s /t 0"
+$trigger = New-ScheduledTaskTrigger -Daily -At "16:00"
+$princibal = New-ScheduledTaskPrincipal -Userid 'gr2.laba304.local\Administrator' -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet 
+$task = New-ScheduledTask -Action $action -Principal $princibal -Trigger $trigger -Settings $settings 
+Register-ScheduledTask Scheduled_Shutdown -InputObject $task
--- userhome.ps1
+++ userhome.ps1
@@ -1,26 +1,5 @@
-	
-param([Parameter(Mandatory=$true)][String]$samAccountName)
- 
-$fullPath = "\\gr2-dc\Home\{0}" -f $samAccountName
-$driveLetter = "Z:"
- 
-$User = Get-ADUser -Identity $samAccountName
- 
-if($User -ne $Null) {
-    Set-ADUser $User -HomeDrive $driveLetter -HomeDirectory $fullPath -ea Stop
-    $homeShare = New-Item -path $fullPath -ItemType Directory -force -ea Stop
- 
-    $acl = Get-Acl $homeShare
- 
-    $FileSystemRights = [System.Security.AccessControl.FileSystemRights]"Modify"
-    $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
-    $InheritanceFlags = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
-    $PropagationFlags = [System.Security.AccessControl.PropagationFlags]"InheritOnly"
- 
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule ($User.SID, $FileSystemRights, $InheritanceFlags, $PropagationFlags, $AccessControlType)
-    $acl.AddAccessRule($AccessRule)
- 
-    Set-Acl -Path $homeShare -AclObject $acl -ea Stop
- 
-    Write-Host ("HomeDirectory created at {0}" -f $fullPath)
-}
+﻿$username = Read-Host 
+
+New-Item -Path "C:\Home\$username" -ItemType Directory
+New-SmbShare -Name $username -Path "C:\Home\$username" -ChangeAccess "gr2.laba304.local\$username"
+SET-ADUser $username -HomeDirectory "\\gr2-dc\$username" -HomeDrive H:
