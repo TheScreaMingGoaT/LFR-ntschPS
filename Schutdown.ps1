936319ca56219d8ab87c67566fc09b2931a21302
TheScreaMingGoaT <paulpaul123paul123@gmail.com>
2024-02-06 11:17:20 +0100

    Ich commite viel zu häufigc2

--- Schutdown.ps1
+++ Schutdown.ps1
@@ -1,6 +1,13 @@
-﻿$action = New-ScheduledTaskAction -Execute "shutdown" -Argument "/s /t 0"
-$trigger = New-ScheduledTaskTrigger -Daily -At "16:00"
+﻿$action = New-ScheduledTaskAction -Execute ".\message.bat"
+$trigger = New-ScheduledTaskTrigger -Daily -At "15:50"
 $princibal = New-ScheduledTaskPrincipal -Userid 'gr2.laba304.local\Administrator' -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet 
 $task = New-ScheduledTask -Action $action -Principal $princibal -Trigger $trigger -Settings $settings 
+Register-ScheduledTask Scheduled_Message -InputObject $task
+
+$actionShutdown = New-ScheduledTaskAction -Execute "shutdown" -Argument "/s /t 0"
+$triggerShutdown = New-ScheduledTaskTrigger -Daily -At "16:00"
+$princibalShutdown = New-ScheduledTaskPrincipal -Userid 'gr2.laba304.local\Administrator' -RunLevel Highest
+$settingsShutdown = New-ScheduledTaskSettingsSet 
+$taskShutdown = New-ScheduledTask -Action $actionShutdown -Principal $princibalShutdown -Trigger $triggerShutdown -Settings $settingsShutdown 
 Register-ScheduledTask Scheduled_Shutdown -InputObject $task
